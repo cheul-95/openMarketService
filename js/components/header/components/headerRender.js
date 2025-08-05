7c7b9f1df5dc9aa1d914eb9750770af268e2a73e
sc.lee <sclee9040@gmail.com>
2025-08-06 08:50:05 +0900

    git path  = url/openMarketService 로인한 경로 설정

--- js/components/header/components/headerRender.js
+++ js/components/header/components/headerRender.js
@@ -4,9 +4,9 @@ export function getHeaderGNB(loginStatus) {
   const $headerMenu = document.querySelector(".header__menu");
   let $headerItem;
   const cart = `<li class="header__menu-item">
-      <a href="../pages/cart.html" class="header__menu-link">
+      <a href="./pages/cart.html" class="header__menu-link">
         <img
-          src="../assets/icons/icon-shopping-cart.svg "
+          src="./assets/icons/icon-shopping-cart.svg "
           alt="장바구니 아이콘"
         />
         <span class="header__menu-text header__menu-cart">장바구니</span>
@@ -26,17 +26,17 @@ export function getHeaderGNB(loginStatus) {
     // 로그인 사용자
     const seller = `<li class="header__menu-item header__seller-item">
         <a href="#" class="header__menu-link">
-          <img src="../assets/icons/icon-seller-button.svg" alt="판매자 센터 아이콘" />
+          <img src="./assets/icons/icon-seller-button.svg" alt="판매자 센터 아이콘" />
         </a>
       </li>`;
     const mypage = `<li class="header__menu-item header__menu-item--mypage dropdown">
         <button type="button" class="header__menu-link dropdown__button">
-          <img src="../assets/icons/icon-user.svg" alt="마이페이지 아이콘" />
+          <img src="./assets/icons/icon-user.svg" alt="마이페이지 아이콘" />
           <span class="header__menu-text">마이페이지</span>
         </button>
         <ul class="dropdown__menu">
           <li class="dropdown__item">
-            <a href="../pages/mypage.html" class="dropdown__link">마이페이지</a>
+            <a href="./pages/mypage.html" class="dropdown__link">마이페이지</a>
           </li>
           <li class="dropdown__item">
             <button class="dropdown__logout">로그아웃</button>
